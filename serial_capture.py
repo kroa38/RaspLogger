e0f3e101da6e9cee1b0cd91f4e430dbfdccada5c
Franck DELPINO <franck.delpino@legrand.fr>
2018-04-27 14:49:53 +0200

    update serial_capture.py

--- serial_capture.py
+++ serial_capture.py
@@ -9,84 +9,88 @@ import serial       # install pyserial package
 # it extract the value of HCHC and HCHP and take care to the CRC.
 # take car about the port ttyAM0 or ttyS0
 
-ser = serial.Serial(
-    port='/dev/ttyS0',
-    baudrate=1200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.SEVENBITS
-)
+def Capture_Linky()
 
-if(ser.is_open):
-    print("port is  opened!")
-    ser.close()
-    ser.open()
-    print (" port was closed and opened again")
+    ser = serial.Serial(
+        port='/dev/ttyS0',
+        baudrate=1200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.SEVENBITS
+    )
 
-nb_line = 11      # la trame historique contient 11 lignes de champs
-HCHC = 3          # HCHC est a la ligne 3
-HCHP = 4          # HPHC est a la ligne 4
-LENGHT_HCHC = 14  # nb de caracteres a retenir
-LENGHT_HCHP = 14  # nb de caracteres
-IMAX = 7
-LENGHT_IMAX = 8
-checksum = True
+    if(ser.is_open):
+        #print("port is  opened!")
+        ser.close()
+        ser.open()
+        #print (" port was closed and opened again")
 
-dico = {}
-dico["HP"] = 0
-dico["HC"] = 0
-dico["IMAX"] = 0
+    nb_line = 11      # la trame historique contient 11 lignes de champs
+    HCHC = 3          # HCHC est a la ligne 3
+    HCHP = 4          # HPHC est a la ligne 4
+    LENGHT_HCHC = 14  # nb de caracteres a retenir
+    LENGHT_HCHP = 14  # nb de caracteres
+    IMAX = 7
+    LENGHT_IMAX = 8
+    checksum = True
 
-while True :
+    Linky = {}
+    Linky["HP"] = 0
+    Linky["HC"] = 0
+    Linky["IMAX"] = 0
 
-        time.sleep(2)
-        out = ''
-        tmp = ''
+    while True :
 
-        while ser.inWaiting() > 0:
-                tmp = ser.read(1)
-                if tmp == chr(2):
-                      break
-        tmp = ser.read()            # avoid chr(2)
-        while ser.inWaiting() > 0:
-                tmp = ser.read(1)
-                if tmp != chr(3):
+            time.sleep(2)
+            out = ''
+            tmp = ''
+
+            while ser.inWaiting() > 0:
+                    tmp = ser.read(1)
+                    if tmp == chr(2):
+                        break
+            tmp = ser.read()            # avoid chr(2)
+
+            while ser.inWaiting() > 0:
+                    tmp = ser.read(1)
+                    if tmp != chr(3):
                         out += tmp
-                else:
-                      break
-        words = out.split(chr(10))
+                    else:
+                        break
+            words = out.split(chr(10))
 
-        if len(words) == nb_line:
-                sum = 0
-                for i in range(LENGHT_HCHP):
+                if len(words) == nb_line:
+
+                    sum = 0
+                    for i in range(LENGHT_HCHP):
                         sum = (sum + ord(words[HCHP][i])) & 0x3F
-                sum = (sum + 0x20) % 256
-                if chr(sum) != words[HCHP][LENGHT_HCHP+1]:
-			checksum = FALSE
-                else:
-                        dico["HP"] = int(words[HCHP].split()[1])
+                    sum = (sum + 0x20) % 256
+                    if chr(sum) != words[HCHP][LENGHT_HCHP+1]:
+                        checksum = FALSE
+                    else:
+                        Linky["HP"] = int(words[HCHP].split()[1])
 
-                sum = 0
-                for i in range(LENGHT_HCHC):
+                    sum = 0
+                    for i in range(LENGHT_HCHC):
                         sum = (sum + ord(words[HCHC][i])) & 0x3F
-                sum = (sum + 0x20) % 256
-                if chr(sum) != words[HCHC][LENGHT_HCHC+1]:
+                    sum = (sum + 0x20) % 256
+                    if chr(sum) != words[HCHC][LENGHT_HCHC+1]:
                         checksum = False
-                else:
-                        dico["HC"] = int(words[HCHC].split()[1])
+                    else:
+                        Linky["HC"] = int(words[HCHC].split()[1])
 
-                sum = 0
-                for i in range(LENGHT_IMAX):
+                    sum = 0
+                    for i in range(LENGHT_IMAX):
                         sum = (sum + ord(words[IMAX][i])) & 0x3F
-                sum = (sum + 0x20) % 256
-                if chr(sum) != words[IMAX][LENGHT_IMAX+1]:
+                    sum = (sum + 0x20) % 256
+                    if chr(sum) != words[IMAX][LENGHT_IMAX+1]:
                         checksum = False
-                else:
-                        dico["IMAX"] = int(words[IMAX].split()[1])
+                    else:
+                        Linky["IMAX"] = int(words[IMAX].split()[1])
 
-                if checksum == True:
-                        print dico
+                    if checksum == True:
                         break;
+    return Linky
 
 
 
