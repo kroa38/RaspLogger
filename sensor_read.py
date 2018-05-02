2f6d63a95dde539ef148994818178e82fde92d36
Franck DELPINO <franck.delpino@legrand.fr>
2018-05-02 17:04:07 +0200

    sensor

--- sensor_read.py
+++ sensor_read.py
@@ -38,12 +38,26 @@ a = 2.602
 b = ZERO_POINT_VOLTAGE
 c = REACTION_VOLTAGE / (2.602 - 3)
 
+
 def calcTmp(ambT, objT):
-    SCALE_LSB = 0.03125
-    it = int(objT >> 2)
-    tObj = float(it)*SCALE_LSB
-    it = int(ambT >> 2)
-    m_tmpAmb = float(it)*SCALE_LSB
+
+    m_tmpAmb = ambT/128.0
+    Vobj2 = objT * 0.00000015625
+    Tdie2 = m_tmpAmb + 273.15
+    S0 = 6.4E-14            # Calibration factor
+    a1 = 1.75E-3
+    a2 = -1.678E-5
+    b0 = -2.94E-5
+    b1 = -5.7E-7
+    b2 = 4.63E-9
+    c2 = 13.4
+    Tref = 298.15
+    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
+    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
+    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
+    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
+    tObj = (tObj - 273.15)
+
     return (m_tmpAmb, tObj)
 
 def calcHum(rawT, rawH):
