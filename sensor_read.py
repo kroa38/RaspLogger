7b5322e3054b52c5f5466709c1858c72e8d98d8b
Franck DELPINO <franck.delpino@legrand.fr>
2018-05-04 13:09:38 +0200

    sensor

--- sensor_read.py
+++ sensor_read.py
@@ -240,7 +240,9 @@ def read_sensor_barometer(handle):
     rawT = long(float.fromhex(baro[2]) * 256 + float.fromhex(baro[1]))
     rawP = long(float.fromhex(baro[4]) * 256 + float.fromhex(baro[3]))
     (temp, pres)= barometer.calc(rawT, rawP)
-    dico = {"Temp_Baro": round(temp,1), "Pressure": round(pres,1)}
+
+    alt = ((pow((1013.25/pres),1/5.257)-1)*(temp+273.15))/0.0065
+    dico = {"Temp_Baro": round(temp,1), "Pressure": round(pres,1), "Alt":round(alt,1)}
     print dico
 
 
