52eb3597b6af34df49f57f5a0dfcd668c59677e5
kroa38 <kroa38@gmail.com>
2018-05-07 10:50:53 +0200

    timout

--- testSerial_Capture.py
+++ testSerial_Capture.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
-
+import RPi.GPIO as GPIO
+import time
 from serial_capture import capture_linky
 
-d = capture_linky()
-print d
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+while True:
+    d = capture_linky()
+    if d["HC"] != 0:
+        GPIO.output(20, 1)
+        time.sleep(0.3)
+        GPIO.output(20, 0)
+    time.sleep(2)
+
