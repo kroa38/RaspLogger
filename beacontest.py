7b9b159ce06cdaf7b4fe30b288e8558b0623c19f
kroa38 <franck.delpino@legrand.fr>
2019-04-12 12:07:27 +0200

    add parseble

--- beacontest.py
+++ beacontest.py
@@ -8,11 +8,12 @@ from beacontools import BeaconScanner, IBeaconFilter
 rtc = DS1338.DS1338(1, 0x68)
 
 def callback(bt_addr, rssi, packet, additional_info):
-    print("%s,%d,%s" % (rtc.read_datetime(),rssi, packet))
-# scan for all iBeacon advertisements from beacons with the specified uuid 
-scanner = BeaconScanner(callback, device_filter=IBeaconFilter(uuid="2332a4c2")
-)
+    print("%s,rssi,%d,%s" % (rtc.read_datetime(),rssi, packet))
+# scan for all iBeacon advertisements from beacons with the specified uuid
+scanner = BeaconScanner(callback, device_filter=IBeaconFilter(uuid="2332a4c2"))
+
 scanner.start()
+
 while True:
     time.sleep(5)
 scanner.stop()
