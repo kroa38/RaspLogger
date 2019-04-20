336fe444eb5ea865b0126de7022ef685771b4898
kroa38 <kroa38@users.noreply.github.com>
2019-04-20 18:28:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 echo "Start of script........................................."
 echo "Package update and upgrade"
-apt-get update
+apt-get update & wait $!
 echo "........................................................"
-apt-get upgrade
+apt-get upgrade -y  & wait $!
 echo "........................................................"
 echo "Install usefull Package"
-apt-get install git
+apt-get install git & wait $!
 echo "........................................................"
-apt-get install python-pip
+apt-get install python-pip & wait $!
 echo "........................................................"
-apt-get install libbluetooth-dev
+apt-get install libbluetooth-dev & wait $!
 echo "........................................................"
-apt-get install apt-transport-https
+apt-get install apt-transport-https & wait $!
 echo "........................................................"
-apt-get install curl
+apt-get install curl & wait $!
 echo "........................................................"
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -  & wait $!
 echo "........................................................"
 echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 echo "........................................................"
-echo "Package update"
+echo "Package update" & wait $!
 echo "........................................................"
-apt-get update
+apt-get update & wait $!
 echo "........................................................"
-apt-get install influxdb
+apt-get install influxdb & wait $!
 echo "........................................................"
 echo "........................................................"
 echo "........................................................"
@@ -32,30 +32,30 @@ echo "Install usefull Python library"
 echo "........................................................"
 echo "........................................................"
 echo "........................................................"
-pip install pybluez
+pip install pybluez & wait $!
 echo "........................................................"
-pip install beacontools
+pip install beacontools & wait $!
 echo "........................................................"
-pip install influxdb
+pip install influxdb & wait $!
 echo "........................................................"
-pip install oauth2client
+pip install oauth2client & wait $!
 echo "........................................................"
-pip install RPi.GPIO
+pip install RPi.GPIO & wait $!
 echo "........................................................"
-pip install requests
+pip install requests & wait $!
 echo "........................................................"
-pip install twitter
+pip install twitter & wait $!
 echo "........................................................"
-pip install smbus
+pip install smbus & wait $!
 echo "........................................................"
-pip install gspread
+pip install gspread & wait $!
 echo "........................................................"
-pip install urllib2
+pip install urllib2 & wait $!
 echo "........................................................"
-pip install httplib2
+pip install httplib2 & wait $!
 echo "........................................................"
-pip install pyserial
+pip install pyserial & wait $!
 echo "........................................................"
-pip install google-api-python-client
+pip install google-api-python-client & wait $!
 echo "........................................................"
 echo "End of script"
