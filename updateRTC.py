616bd61cd392ee9787d87ec916203ec84cf1daef
kroa38 <kroa38@users.noreply.github.com>
2019-05-04 18:22:34 +0200

    Update updateRTC.py

--- updateRTC.py
+++ updateRTC.py
@@ -5,12 +5,14 @@ import DS1338
 from util_funct import check_internet, log_event, tweet_message
 
 # Main Program
+if __name__ == "__main__":
+    
+    
+    rtc = DS1338.DS1338(1, 0x68)
 
-rtc = DS1338.DS1338(1, 0x68)
-
-if check_internet() == "1":
-    rtc.write_now()
-    rtc.write_ctrl()
-    log_event("rtc updated")
-    tweet_message("rtc updated")
+    if check_internet() == "1":
+        rtc.write_now()
+        rtc.write_ctrl()
+        log_event("rtc updated")
+        tweet_message("rtc updated")
 
