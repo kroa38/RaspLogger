fba92bbc8939d1b232bf03f10daf647207392820
kroa38 <kroa38@gmail.com>
2019-05-05 18:55:43 +0200

    update

--- tweet_ip.py
+++ tweet_ip.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-#this code is used to tweet the current local ip address, because
-#in dhcp mode we don't know it.
 
-from cloudscope import tweet_ip_addr, check_internet
+from util_funct import tweet_ip_address, check_internet
+
 
 def tweet_ip():
-	"""
-	check if internet is up.
-	tweet the ip address
-	"""
-	if check_internet()=="1":
-		tweet_ip_addr()
-	exit()
+    """" check if internet is up.
+         tweet the ip address
+    """
+    if check_internet()==True:
+        tweet_ip_address()
 
-tweet_ip()
 
