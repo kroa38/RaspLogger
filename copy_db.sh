5849e870c6564bc902564ac9c05a803c0040e34a
kroa38 <kroa38@users.noreply.github.com>
2019-06-07 15:49:50 +0200

    Delete copy_db.sh

--- copy_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# make a copy of the database to a USB Key
-# Test if USB key is mounted in /home/pi/USB_KEY
-# install this script in cron and start it every day.
-
-if [ 'mount | grep USB_KEY' ]
-then
-   cp -ru /home/pi/influxdb/ /home/pi/USB_KEY/.
-fi
