2b85bb5c7643c5627523bbcf12feb2f8c1b83de1
kroa38 <kroa38@users.noreply.github.com>
2019-06-23 11:23:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,31 +3,31 @@ echo "##########################################################################
 echo "Start of script..............................................................."
 echo "##############################################################################"
 echo "Package update ..............................................................."
-apt-get update & wait $!
+sudo apt-get update & wait $!
 echo "##############################################################################"
 echo "Package upgrade ..............................................................."
-apt-get upgrade -y  & wait $!
+sudo apt-get upgrade -y  & wait $!
 echo "##############################################################################"
 echo "install git..................................................................."
-apt-get -y install git & wait $!
+sudo apt-get -y install git & wait $!
 echo "##############################################################################"
 echo "install apt-transport-https..................................................."
-apt-get -y install apt-transport-https & wait $!
+sudo apt-get -y install apt-transport-https & wait $!
 echo "##############################################################################"
 echo "install bluetooth............................................................."
-apt-get -y install bluetooth & wait $!
+sudo apt-get -y install bluetooth & wait $!
 echo "##############################################################################"
 echo "install libbluetooth-dev......................................................"
-apt-get -y install libbluetooth-dev & wait $!
+sudo apt-get -y install libbluetooth-dev & wait $!
 echo "##############################################################################"
 echo "install python-dev............................................................"
-apt-get -y install python-dev & wait $!
+sudo apt-get -y install python-dev & wait $!
 echo "##############################################################################"
 echo "install python-pip............................................................"
-apt-get -y install python-pip & wait $!
+sudo apt-get -y install python-pip & wait $!
 echo "##############################################################################"
 echo "install curl.................................................................."
-apt-get -y install curl & wait $!
+sudo apt-get -y install curl & wait $!
 echo "##############################################################################"
 echo ".............................................................................."
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -  & wait $!
@@ -36,23 +36,23 @@ echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/ap
 echo ".............................................................................."
 echo "##############################################################################"
 echo "Package update" & wait $!
-apt-get update & wait $!
+sudo apt-get update & wait $!
 echo "##############################################################################"
 echo "install influxdb.............................................................."
-apt-get -y install influxdb & wait $!
+sudo apt-get -y install influxdb & wait $!
 echo "##############################################################################"
 echo ".............INSTALL PYTHON PACKAGES .........................................."
 echo "##############################################################################"
-pip install -r requirements.txt & wait $!
+sudo pip install -r requirements.txt & wait $!
 echo "##############################################################################"
 echo ".............SYSTEM TIPS AND TRICKS .........................................."
 echo "##############################################################################"
 echo "##############################################################################"
 echo "Copy timezone file ..........................................................."
-cp -f /home/pi/RaspLogger/etc/timezone /etc/.
+sudo p -f /home/pi/RaspLogger/etc/timezone /etc/.
 echo "##############################################################################"
 echo "Copy cron file ..........................................................."
-cp -rf /home/pi/RaspLogger/cron/root /var/spool/cron/crontabs/.
+sudo cp -rf /home/pi/RaspLogger/cron/root /var/spool/cron/crontabs/.
 echo "##############################################################################"
 echo "Restart cron service .................... ......................................."
 sudo service cron restart & wait $!
@@ -61,46 +61,46 @@ echo "Copy sshd_config file ....................................................
 cp -rf /home/pi/RaspLogger/ssh/sshd_config /etc/ssh/.
 echo "##############################################################################"
 echo "Reload SSH service ..........................................................."
-systemctl reload ssh.service & wait $!
+sudo systemctl reload ssh.service & wait $!
 echo "##############################################################################"
 echo "Copy influxdb.conf file ..........................................................."
-cp -rf /home/pi/RaspLogger/influxdb/influxdb.conf /etc/influxdb/.
+sudo cp -rf /home/pi/RaspLogger/influxdb/influxdb.conf /etc/influxdb/.
 echo "##############################################################################"
 echo "Create influxdb directory Wal,Data,Meta
 if [ ! -d /home/pi/USB_KEY/influxdb ]
 then
-    mkdir /home/pi/USB_KEY/influxdb
+    sudo mkdir /home/pi/USB_KEY/influxdb
 fi
 
 if [ ! -d /home/pi/USB_KEY/influxdb/meta ]
 then
-    mkdir /home/pi/USB_KEY/influxdb/meta
+   sudo mkdir /home/pi/USB_KEY/influxdb/meta
 fi
 
 if [ ! -d /home/pi/USB_KEY/influxdb/data ]
 then
-    mkdir /home/pi/USB_KEY/influxdb/data
+    sudo mkdir /home/pi/USB_KEY/influxdb/data
 fi
 
 if [ ! -d /home/pi/USB_KEY/influxdb/wal ]
 then
-    mkdir /home/pi/USB_KEY/influxdb/wal
+    sudo mkdir /home/pi/USB_KEY/influxdb/wal
 fi
 chown -R influxdb:influxdb /home/pi/influxdb
 echo "##############################################################################"
 echo "Create Users in database......................................................"
 if [ -f /home/pi/RaspLogger/credential.txt ]
 then
-	python util_dbase.py & wait $!
+	sudo python util_dbase.py & wait $!
 else
 	echo "No credential file  : Database Users not created"
 fi
 echo "##############################################################################"
 echo "Restart Influxdb......................................................"
-service influxdb restart & wait $!
+sudo service influxdb restart & wait $!
 echo "##############################################################################"
 echo "Enable Influxdb......................................................"
-systemctl enable influxdb.service & wait $!
+sudo systemctl enable influxdb.service & wait $!
 echo "##############################################################################"
 echo "End of script................................................................."
 echo "##############################################################################"
