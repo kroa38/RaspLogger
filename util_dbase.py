dd62e154b69b6a2d718b47d88c9e9c9cf85539d3
kroa38 <kroa38@users.noreply.github.com>
2019-06-30 15:36:07 +0200

    Update util_dbase.py

--- util_dbase.py
+++ util_dbase.py
@@ -10,24 +10,42 @@ def write_to_dbase(jsony_body,db_name):
     """
     currentpathdir = os.path.dirname(os.path.realpath(__file__))
     cred_file = os.path.join(currentpathdir, "credential.txt")
-    data_json = get_json_data_from_file(cred_file)
+    try:
+        data_json = get_json_data_from_file(cred_file)
+    except:
+	print("**********************************")
+	print("* Error : no file credential.txt *")	
+	print("**********************************")
+	return
     db_user = data_json['DATABASE_USER_ADMIN']
     db_password = data_json['DATABASE_PASSWORD_ADMIN']
     client = InfluxDBClient('localhost', 8086, db_user, db_password, db_name)
     dbs = client.get_list_database()
     d = next((d for d in dbs if d['name'] == db_name), None)
     if d is None:
-        log_event("Create database : %s " % db_name )
         client.create_database(db_name)
+        if db_name == 'sysinfo':
+            client.create_retention_policy('autogen','7d','1',db_name,False,'7d')      
+        else:
+            client.create_retention_policy('autogen','0s','1',db_name,False,'31d') 
+        
+        # grant privilege for grafana user
+        db_user = data_json['DATABASE_USER_READER']
+        client.grant_privilege('read',db_name,db_user)	        
+    
     result = client.write_points(jsony_body)
     if not result:
         log_error("Database write error")
+
 
 def init_dbase():
     """
     :param none
     :return: none
     """
+	print("**********************************")
+	print("* INIT DBASE USERS               *")	
+	print("**********************************")
     currentpathdir = os.path.dirname(os.path.realpath(__file__))
     cred_file = os.path.join(currentpathdir, "credential.txt")
     try:
@@ -38,40 +56,23 @@ def init_dbase():
 	print("**********************************")
 	return
 	
-    db_user = data_json['DATABASE_USER_ADMIN']
-    db_password = data_json['DATABASE_PASSWORD_ADMIN']
-    client = InfluxDBClient('localhost', 8086)
-	
-    # create databases	
-    client.create_database('linky')
-    client.create_database('air_quality')
-    client.create_database('ibeacon')
-    client.create_database('sysinfo')
-    client.create_database('gpio')
-	
-    # set retention policies for databases
-    client.create_retention_policy('autogen','0s','1','linky',False,'31d')
-    client.create_retention_policy('autogen','0s','1','air_quality',False,'31d')
-    client.create_retention_policy('autogen','0s','1','ibeacon',False,'31d')	
-    client.create_retention_policy('autogen','7d','1','sysinfo',False,'7d')	
-    client.create_retention_policy('autogen','0s','1','gpio',False,'31d')	
-	
+    client = InfluxDBClient('localhost', 8086)     
+	print("**********************************")
+	print("* CREATE INFLUXDB GRAFANA USER   *")	
+	print("**********************************")   
+    # create a user for grafana	
+    db_user = data_json['DATABASE_USER_READER']
+    db_password = data_json['DATABASE_PASSWORD_READER']	
+    client.create_user(db_user,db_password,admin=False)	
+ 
+	print("**********************************")
+	print("* CREATE INFLUXDB ADMIN USER     *")	
+	print("**********************************")   
     # create admin user with read/write privilege	
     db_user = data_json['DATABASE_USER_ADMIN']
     db_password = data_json['DATABASE_PASSWORD_ADMIN']	
     client.create_user(db_user,db_password,admin=True)
 
-    # create a user for grafana	
-    db_user = data_json['DATABASE_USER_READER']
-    db_password = data_json['DATABASE_PASSWORD_READER']	
-    client.create_user(db_user,db_password,admin=False)	
-	
-    # set read privilege for grafana user  
-    client.grant_privilege('read','ibeacon',db_user)
-    client.grant_privilege('read','linky',db_user)
-    client.grant_privilege('read','air_quality',db_user)
-    client.grant_privilege('read','sysinfo',db_user)
-    client.grant_privilege('read','gpio',db_user)
 
 
 if __name__ == "__main__":
