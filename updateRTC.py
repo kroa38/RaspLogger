b4f0becc1da2b48374ba1e80e48014de38a0bf66
kroa38 <kroa38@users.noreply.github.com>
2021-02-27 09:49:22 +0100

    update for python3

--- updateRTC.py
+++ updateRTC.py
@@ -6,8 +6,7 @@ from util_funct import check_internet, log_event, tweet_message
 
 # Main Program
 if __name__ == "__main__":
-    
-    
+
     rtc = DS1338.DS1338(1, 0x68)
 
     if check_internet() == "1":
@@ -15,4 +14,3 @@ if __name__ == "__main__":
         rtc.write_ctrl()
         log_event("rtc updated")
         tweet_message("rtc updated")
-
