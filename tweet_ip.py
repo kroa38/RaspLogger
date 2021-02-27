54ea4f1f41a5be014ac245ee85cb9528f1d3e578
kroa38 <kroa38@users.noreply.github.com>
2021-02-27 09:48:42 +0100

    update for python3

--- tweet_ip.py
+++ tweet_ip.py
@@ -7,8 +7,9 @@ def tweet_ip():
     """" check if internet is up.
          tweet the ip address
     """
-    if check_internet()==True:
+    if check_internet():
         tweet_ip_address()
+
 
 if __name__ == "__main__":
     tweet_ip()
