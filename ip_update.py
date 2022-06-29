bbc6dfa20770e896d4aa2521d007b92836b61c3b
kroa38 <kroa38@users.noreply.github.com>
2022-06-29 18:37:22 +0200

    Update ip_update.py

--- ip_update.py
+++ ip_update.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     for example every hour
     0 * * * * python /this_script.py > /dev/null 2>&1
     '''
-    debug_print = True
+    debug_print = False
     opendns()
     duckdns()
 
