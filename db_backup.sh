5d94324be7760703cb1a31ef92f57b6b6ad9be6f
Baby Bubba <kroa38@users.noreply.github.com>
2022-11-30 19:10:34 +0100

    Update db_backup.sh

--- db_backup.sh
+++ db_backup.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-
-# Backup database to USB SSD flash disk
-# First test if Usb SSD is mounted
-# then make a secure copy
+# Backup database to DB_BACKUP directory
+# make sure dir home/pi/DB_BACKUP exist
+# make sure dir home/pi/DB_BACKUP/tmp exist
+# or modify this script to create and test theses dir...
 
 echo "***********************************"
 echo "         BACKUP DATABASE           "
 echo "***********************************"
-if [ 'mount | grep USB_SSD & wait $!' ]
+
+echo "USB key is present"
+echo "start backup"
+influxd backup -portable /home/pi/DB_BACKUP/tmp/ > db_backup.log
+if [ 'cat db_backup.log | grep "backup complete" ' ]
 then
-    echo "USB SSD is present"
-    echo "start backup"
-    influxd backup -portable /home/pi/USB_SSD/tmp/ > db_backup.log
-    if [ 'cat db_backup.log | grep "backup complete" ' ]
+    echo "influxd backup ok"
+    if [ -d "/home/pi/DB_BACKUP/influxdb_backup" ]
     then
-        echo "influxd backup ok"
-        if [ -d "/home/pi/USB_SSD/influxdb_backup" ]
-        then
-            echo "remove and rename directory"
-            rm -rf /home/pi/USB_SSD/influxdb_backup
-            mv /home/pi/USB_SSD/tmp /home/pi/USB_SSD/influxdb_backup
-        else
-            echo "move only directory"
-            mv /home/pi/USB_SSD/tmp /home/pi/USB_SSD/influxdb_backup
-        fi
+        echo "remove and rename directory"
+        rm -rf /home/pi/DB_BACKUP/influxdb_backup
+        mv /home/pi/DB_BACKUP/tmp /home/pi/DB_BACKUP/influxdb_backup
     else
-        echo "influd backup fail"
+        echo "move only directory"
+        mv /home/pi/DB_BACKUP/tmp /home/pi/DB_BACKUP/influxdb_backup
     fi
 else
-echo "USB SSD not present ! "
+    echo "influd backup fail"
 fi
+
 echo "END BACKUP"
 echo "***********************************"
+
